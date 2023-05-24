4b6e32de994ce2520755565d0f9cc479782cc2aa
Tianhao-web <tianhaol2@student.unimelb.edu.au>
2023-05-24 13:21:26 +1000

    changing the file reading path

--- data_processing_mapreduce & upload_to_couchdb/sudo_data_upload/Data Processing_SUDO.py
+++ data_processing_mapreduce & upload_to_couchdb/sudo_data_upload/Data Processing_SUDO.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
-df = pd.read_csv('EducationLevel_SUDO_after cleaning.csv')
+df = pd.read_csv('../graph_data/EducationLevel_SUDO_after cleaning.csv')
 jdf = df.to_json(orient='records')
 #records = json.loads(jdf)
-with open('EducationLevel_SUDO_after cleaning.json', 'w') as f:
+with open('../graph_data/EducationLevel_SUDO_after cleaning.json', 'w') as f:
     f.write(jdf)
 df['X'] = df.index.values
 
-with open('EducationLevel_SUDO_after cleaning.json', 'r') as f:
+with open('../graph_data/EducationLevel_SUDO_after cleaning.json', 'r') as f:
     for line in f:
         records = json.loads(jdf)
         
