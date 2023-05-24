c72cd44d30c4ff8a48f5fee8c6af62ed5e01c849
Tianhao-web <tianhaol2@student.unimelb.edu.au>
2023-05-24 11:32:42 +1000

    change notebook to python

---
+++ data_processing_mapreduce & upload_to_couchdb/bad_lang_tweet_mapreduce.py
@@ -0,0 +1,41 @@
+import couchdb
+import json
+username='group6'
+passwd = '123456'
+ip = '172.26.131.122'
+hostName='http://{}:{}@{}:5984/'.format(username, passwd, ip)
+couch = couchdb.Server(hostName)
+db = couch['bad_lang_tweet']
+design_doc="countGccLang5"
+def mapreduce(db):
+    mapFunc = '''function(doc) {
+                emit([doc.gcc, doc.doc.data.lang], 1);
+    }'''
+    reduceFunc = 'function(keys, values) { return sum(values); }'
+    ddoc = {
+        "_id": "_design/{}".format(design_doc),
+        "views": {
+            "my_view": {
+                "map": mapFunc,
+                "reduce": reduceFunc
+            }
+        }
+    }
+    db.save(ddoc)
+mapreduce(db)
+import requests
+
+url = "http://172.26.131.122:5984/bad_lang_tweet/_design/countGccLang5/_view/my_view?group=true"
+username = "group6"
+password = "123456"
+
+response = requests.get(url, auth=(username, password))
+
+if response.status_code != 200:
+    print(f"Error: {response.status_code}")
+    
+file_name = "../graph_data/count_gcc_lang_bad.json"
+
+# Write the JSON object to a file
+with open(file_name, "w") as file:
+    json.dump(data, file, indent=4)
---
+++ data_processing_mapreduce & upload_to_couchdb/count_lang_tweet_mapreduce.py
@@ -0,0 +1,41 @@
+import couchdb
+import json
+username='group6'
+passwd = '123456'
+ip = '172.26.131.122'
+hostName='http://{}:{}@{}:5984/'.format(username, passwd, ip)
+couch = couchdb.Server(hostName)
+db = couch['count_lang_tweet']
+design_doc="count_gcc_lang"
+def mapreduce(db):
+    mapFunc = '''function(doc) {
+                emit([doc.gcc, doc.doc.data.lang], 1);
+    }'''
+    reduceFunc = 'function(keys, values) { return sum(values); }'
+    ddoc = {
+        "_id": "_design/{}".format(design_doc),
+        "views": {
+            "my_view": {
+                "map": mapFunc,
+                "reduce": reduceFunc
+            }
+        }
+    }
+    db.save(ddoc)
+mapreduce(db)
+import requests
+
+url = "http://172.26.131.122:5984/count_lang_tweet/_design/count_gcc_lang/_view/my_view?group=true"
+username = "group6"
+password = "123456"
+
+response = requests.get(url, auth=(username, password))
+
+if response.status_code != 200:
+    print(f"Error: {response.status_code}")
+
+file_name = "../graph_data/group_gcc_lang.json"
+
+# Write the JSON object to a file
+with open(file_name, "w") as file:
+    json.dump(data, file, indent=4)
