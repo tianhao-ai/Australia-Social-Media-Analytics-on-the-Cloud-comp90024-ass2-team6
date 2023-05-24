f95f65da12898f9f5e85bf3ba235ed8e27d6553c
Tianhao-web <tianhaol2@student.unimelb.edu.au>
2023-05-24 17:07:47 +1000

    changing notebook to py

---
+++ SUDO_Data/education_level_from_sudo/Data Processing_SUDO.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+df = pd.read_csv('EducationLevel_SUDO_after cleaning.csv')
+jdf = df.to_json(orient='records')
+#records = json.loads(jdf)
+with open('EducationLevel_SUDO_after cleaning.json', 'w') as f:
+    f.write(jdf)
+df['X'] = df.index.values
+
+with open('EducationLevel_SUDO_after cleaning.json', 'r') as f:
+    for line in f:
+        records = json.loads(jdf)
+        
+ax = df.plot(x = 'X', y=["Bachelar (%)", ' Postgraduate (%)', "Others (%)"], kind="bar", rot=90, stacked=True)
+_ = ax.legend(bbox_to_anchor=(1, 1.02), loc='upper left')
+ax.set_xticklabels(df[' gccsa_name_2016'].values.tolist())
